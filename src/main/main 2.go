32e6622770ed9389434d0e74f7b6d3ea21d82727
linzhaojie <598332351@qq.com>
2019-01-17 21:29:23 +0800

    test1

---
+++ src/main/main 2.go
@@ -0,0 +1,13 @@
+package models
+
+import(
+    "log"
+)
+func main(){
+	log.println("测试1")
+	log.println("测试2")
+	log.println("测试3")
+	log.println("测试4")
+	log.println("测试5")
+	log.println("测试6")
+}
